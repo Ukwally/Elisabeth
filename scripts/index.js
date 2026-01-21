d2b7ceb5698b27ad1dfc25b49fa0f79fab0e24e4
ukwally <ukwally22rgmail.com>
2026-01-21 09:55:59 +0000

    melhorar interface do more

--- scripts/index.js
+++ scripts/index.js
@@ -111,7 +111,7 @@ function contarTempo() {
 
 function contarTempo() {
     const inicio = Date.now();
-    const final = new Date('2026-02-02').getTime();
+    const final = new Date('2026-03-02').getTime();
     const falta = final - inicio;
 
     const segundos = Math.floor(falta / 1000) % 60;
